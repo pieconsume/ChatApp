eab94b22fb66c6706e42e498703e9f55f40edb2d
pieconsume <130113511+pieconsume@users.noreply.github.com>
2024-03-01 15:42:23 -0500

    Replace divs with textareas when applicable
    Goodbye div-only design... :(

--- Host.js
+++ Host.js
@@ -1,6 +1,10 @@
 const { WebSocketServer } = require("ws");
 const server = new WebSocketServer({ port: 6075 });
 server.on("connection", client => {
- client.on("message", (msg) => { server.clients.forEach(c => c.send(msg)); });
+ client.on("message", (msg) => {
+  if (msg.isBinary) { return; }
+  if (msg.length > 400) { client.send("Message too long"); return; }
+  server.clients.forEach(c => c.send(msg + "")); 
+  });
  });
 console.log("Hosting");
